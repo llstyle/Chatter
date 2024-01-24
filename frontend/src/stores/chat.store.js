1fd6db014aa3aa8d79d9bc6b3a32e8feb15f693b
llstyle <weredhol@gmail.com>
2024-01-24 21:40:58 +0200

    bugs fix frontend

--- frontend/src/stores/chat.store.js
+++ frontend/src/stores/chat.store.js
@@ -8,10 +8,10 @@ export const useChatStore = defineStore('chat', () => {
 
     const chatsFiltered = computed(() => {
         chats.value.sort((a, b) => {
-            return new Date(b.message?.at(0).createdAt) - new Date(a.message?.at(0).createdAt);
+            return new Date(b.message?.at(0)?.createdAt) - new Date(a.message?.at(0)?.createdAt);
         })
         return chats.value
     })
-    
+
     return { chat, chats, messages, chatsFiltered  }
   })
