ba745d2c329eca2fcf1df1fa829f50cbb08fa7f0
llstyle <weredhol@gmail.com>
2024-05-18 18:11:14 +0300

    redis fix

--- backend/utils/notification-util.js
+++ backend/utils/notification-util.js
@@ -1,39 +1,42 @@
 import { createClient } from 'redis';
+import logger from './logger.js';
 
 const URL_REDIS_CONN = process.env.URL_REDIS_CONN
+const publisher = createClient({ url: URL_REDIS_CONN });
+
 export async function sendMessage(message, users) {
     try {
-        const publisher = createClient({ url: URL_REDIS_CONN });
         await publisher.connect()
         await publisher.publish("message", JSON.stringify({message, users}))
+        await publisher.disconnect()
     } catch (e) {
-        console.log(e)
+        logger.error(e.message)
     }
 }
 export async function sendMessageTopic(message, topic) {
     try {
-        const publisher = createClient({ url: URL_REDIS_CONN });
         await publisher.connect()
         await publisher.publish("messageTopic", JSON.stringify({message, topic}))
+        await publisher.disconnect()
     } catch (e) {
-        console.log(e)
+        logger.error(e.message)
     }
 }
 export async function subcribeTopic(users, topic) {
     try {
-        const publisher = createClient({ url: URL_REDIS_CONN });
         await publisher.connect()
         await publisher.publish("subscribe", JSON.stringify({users, topic}))
+        await publisher.disconnect()
     } catch (e) {
-        console.log(e)
+        logger.error(e.message)
     }
 }
 export async function unsubcribeTopic(users, topic) {
     try {
-        const publisher = createClient({ url: URL_REDIS_CONN });
         await publisher.connect()
         await publisher.publish("unsubscribe", JSON.stringify({users, topic}))
+        await publisher.disconnect()
     } catch (e) {
-        console.log(e)
+        logger.error(e.message)
     }
 }
